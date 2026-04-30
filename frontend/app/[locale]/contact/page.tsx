35c28f821fe62ce0023f71b2d0fa4653c48168a8
kanikarun <kanikarun0@hmail.com>
2026-04-30 10:27:41 +0700

    fix: metadata for contact

--- frontend/app/[locale]/contact/page.tsx
+++ frontend/app/[locale]/contact/page.tsx
@@ -1,7 +1,13 @@
 import { Contact } from "@/modules/contact";
-
+import { Metadata } from "next";
 export async function ContactPage() {
   return <Contact />;
 }
+
+export const metadata: Metadata = {
+  title: "Contact",
+  description:
+    "Get in touch with OneWorld Software, a leading software development company in Cambodia. Contact us for web and mobile app development services.",
+};
 
 export default ContactPage;
