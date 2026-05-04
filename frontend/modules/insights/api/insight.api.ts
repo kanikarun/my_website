817ccc1561ffcd46e0aed7f93ccecac81180ee2b
kanikarun <kanikarun0@hmail.com>
2026-05-04 10:35:26 +0700

    fix:Insight km language

--- frontend/modules/insights/api/insight.api.ts
+++ frontend/modules/insights/api/insight.api.ts
@@ -1,27 +1,34 @@
-'use server';
+"use server";
 
-import { Locale } from 'next-intl';
+import { Locale } from "next-intl";
 
-import { fetchAPI, getStrapiMedia, Pagination, STRAPI_IMAGE_FIELDS } from '@/strapi';
-import { getAttributes } from '@/strapi/helper';
+import {
+  fetchAPI,
+  getStrapiMedia,
+  Pagination,
+  STRAPI_IMAGE_FIELDS,
+} from "@/strapi";
+import { getAttributes } from "@/strapi/helper";
 
-import * as I from './insight.interface';
+import * as I from "./insight.interface";
 
-export async function getInsights(opt: I.GetInsightsRequest): Promise<I.GetInsightsResponse> {
-  const { locale = 'en', notId, page, pageSize } = opt;
+export async function getInsights(
+  opt: I.GetInsightsRequest,
+): Promise<I.GetInsightsResponse> {
+  const { locale = "en", notId, page, pageSize } = opt;
   const filterByNotId = notId ? { id: { $not: notId } } : {};
 
-  const fields = ['title', 'slug', 'createdAt', 'description', 'locale'];
-  const { data, meta } = await fetchAPI<I.InsightResponse>('/insights', {
+  const fields = ["title", "createdAt", "description", "locale"];
+  const { data, meta } = await fetchAPI<I.InsightResponse>("/insights", {
     filters: { ...filterByNotId },
     pagination: { page: +(page || 1), pageSize: +(pageSize || 12) },
-    sort: ['id:desc'],
-    fields,
+    sort: ["id:desc"],
+    fields: ["title", "description"],
     populate: {
       image: STRAPI_IMAGE_FIELDS,
-      insight_categories: { fields: ['name', 'slug'] },
-      localizations: { fields }
-    }
+      insight_categories: { fields: ["name", "slug"] },
+      localizations: { fields },
+    },
   });
 
   const result = data.map(({ id, attributes }) => {
@@ -32,38 +39,51 @@ export async function getInsights(opt: I.GetInsightsRequest): Promise<I.GetInsig
       id: id,
       title: attr.title,
       description: attr.description,
-      slug: attr.slug,
       image: getStrapiMedia(attr.image),
-      categories: attr.insight_categories.data.map(x => x.attributes.name)
+      categories:
+        attr.insight_categories?.data?.map((x: any) => x.attributes.name) ??
+        [],
     };
   });
 
   return { data: result, meta };
 }
 
-export async function getInsightDetailById(id: number, locale: Locale): Promise<I.IInsight | null> {
-  const res = await fetchAPI<I.InsightResponse>('/insights', {
-    locale,
+export async function getInsightDetailById(
+  id: number,
+  locale: Locale,
+): Promise<I.IInsight | null> {
+  const res = await fetchAPI<I.InsightResponse>("/insights", {
+    locale: "en",
     filters: { id: { $eq: id } },
-    populate: '*',
-    pagination: { page: 1, pageSize: 1 }
+    populate: {
+      image: "*",
+      insight_categories: { fields: ["name", "slug"] },
+      localizations: { fields: ["*"] },
+    },
+    pagination: { page: 1, pageSize: 1 },
   });
 
-  if (locale !== 'en' && !res.data.length) {
-    return getInsightDetailById(id, 'en');
-  }
+  if (!res.data.length) return null;
 
-  return res.data.length ? res.data[0].attributes : null;
+  const { attributes: attr } = res.data[0];
+  const localize = attr.localizations?.data
+    ? getAttributes(attr.localizations, locale)
+    : undefined;
+
+  return { ...attr, ...localize?.attributes };
 }
 
 export async function updateInsightViewCount(slug: string) {
-  await fetchAPI(`/insights/${slug}/views-count`, {}, { method: 'POST' });
+  await fetchAPI(`/insights/${slug}/views-count`, {}, { method: "POST" });
 }
 
-export async function getInsightCategories(pagination: Pagination): Promise<I.InsightCategoriesResponse> {
+export async function getInsightCategories(
+  pagination: Pagination,
+): Promise<I.InsightCategoriesResponse> {
   return fetchAPI(`/insight-categories`, {
-    sort: ['id:desc'],
-    fields: ['name', 'slug'],
-    pagination
+    sort: ["id:desc"],
+    fields: ["name", "slug"],
+    pagination,
   });
 }
